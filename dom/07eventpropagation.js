a9475e6c913badb855b44610de11dd6d7bada62c
myukxmyuk <reymundcjumawan+github@gmail.com>
2022-10-30 17:52:49 +0800

    Update Event Propagation

--- dom/07eventpropagation.js
+++ dom/07eventpropagation.js
@@ -1,24 +1,16 @@
-document.querySelector("#grand-parent").addEventListener(
-  "click",
-  () => {
-    console.log("Grandparent Clicked");
-  },
-  true
-); // false is the default value that is the event bubbling. It is the third parameter which is useCapture. if sets to true, then the propagation is event capturing
+document
+  .querySelector("#grand-parent")
+  .addEventListener("click", () => console.log("Grandparent Clicked"), true); // false is the default value that is the event bubbling. It is the third parameter which is useCapture. if sets to true, then the propagation is event capturing
 
-document.querySelector("#parent").addEventListener(
-  "click",
-  (e) => {
-    console.log("Parent Clicked");
-  },
-  false
-);
+document
+  .querySelector("#parent")
+  .addEventListener("click", () => console.log("Parent Clicked"), false);
 
 document.querySelector("#child").addEventListener(
   "click",
   (e) => {
     console.log("Child Clicked");
-    e.stopPropagation();
+    e.stopPropagation(); // stopPropagation stops the event and wont bubble nor capture
   },
   true
 );
