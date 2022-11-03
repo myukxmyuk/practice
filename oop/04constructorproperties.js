0ed016586cc2143bffa679c2b6ee7963cb44090c
myukxmyuk <reymundcjumawan+github@gmail.com>
2022-11-03 17:34:33 +0800

    Add Adding and Removing Properties

--- oop/04constructorproperties.js
+++ oop/04constructorproperties.js
@@ -2,7 +2,7 @@
 
 function createCircle(radius) {
   return {
-    radius, // this is equal to radius: radius, in ES6 we can remove the noise and make it simple if key and value name is the same
+    radius, // this line is equal to radius: radius, in ES6 we can remove the noise and make it simple if key and value pair names are the same
     draw: function () {
       console.log("draw");
     },
@@ -23,8 +23,10 @@ function Circle(radius) {
 const another = new Circle(1); // creates an empty object like this {}
 another.draw();
 
-// every object in javascript has a property called constructor, references the function that was use to construct or create the object
+// every object in javascript has a constructor property, references the function that was use to construct or create the object
 // go to console in browser and type another.constructor
 
 new String(); // the constructor is new String(), the string literal are '', "", ``
 new Boolean(); // the constructor is new Boolean(), the boolean literal are true or false
+
+// Functions are Objects
---
+++ oop/05addingremovingproperties.js
@@ -0,0 +1,18 @@
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+const circle = new Circle(10);
+
+// Adding property
+
+circle.location = { x: 1 }; // property name location is added
+console.log(circle); // display all properties
+
+// Or we add a dynamic name of property like this
+
+const propertyName = "location";
+circle[propertyName] = { x: 1 };
+console.log(circle); // display all properties
